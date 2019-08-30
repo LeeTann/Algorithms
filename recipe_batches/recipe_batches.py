d35624fc8671ba1d3703ea5490e336a3e5273726
ltann <leeroyleeroy18@gmail.com>
2019-08-29 19:33:57 -0700

    Completed stock prices and recipe batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python
 
+# ingredients divide by recipe = total pies
+# if any ingredients is missing return 0
+
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  total = None
 
+  # Loop thru the array recipe
+  for i in recipe:
+    print(i)
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Chech to see if items are in ingredients array
+    if i in ingredients:
+
+      # If so, divide ingredients numbers by recipes numbers
+      current_total = ingredients[i] // recipe[i]
+      print("current totals", current_total)
+
+      if total == None:
+        total = current_total
+        print("total:", total)
+
+      elif total > current_total:
+        total = current_total
+    else:
+      return 0
+  
+  return total
+
+print(recipe_batches(
+  { 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }
+))
+
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,33 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # Keep track of max profit
+  min_price = prices[0]
+  print("min price = ", min_price)
+  max_profit = prices[1] - min_price
+  print("max profit = ", max_profit)
 
+  # Loop thru the array with first pointer and get all values
+  for i in range(len(prices)):
+    print("i varible =", prices[i])
+
+    # Loop thru the array again with second pointer to get second values to compare
+    for j in range(i + 1, len(prices)):
+      print("j variable", prices[j])
+
+      # Get the difference/current profit which is second pointer minus first pointer
+      current_profit = prices[j] - prices[i]
+      print("current profit =", current_profit)
+
+      # If current profit > max profit then set max profit = current profit
+      if current_profit > max_profit:
+        print(f"price_j {prices[j]} - price i {prices[i]}")
+        max_profit = current_profit
+        print("new max profit = ", max_profit)
+
+  return max_profit
+  
+print(find_max_profit([1050, 270, 1540, 3800, 2])) 
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
