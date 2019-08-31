1a7a2cf4bcaec8f35f3fdd15dcc36917e7da0df1
ltann <leeroyleeroy18@gmail.com>
2019-08-30 21:20:51 -0700

    finished eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,12 +5,40 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+# 0(n) - Linear when using memcache. 
+# 0(2^n) - when not using memcache
+
+# For memchache you usually want to implement a dict outside the function
+# For the dict then, memchase[n] would be the key, outcome would be value
+memcache = {}
+
+def eating_cookies(n, memcache=None):
+  # check if memcache has a value first, if it does we can just return
+  if n in memcache:
+    # memcache[n] is every value thats been called before
+    return memcache[n]
+
+  if n < 0:
+    return -1
+  if n <= 1:
+    return 1
+  if n == 2:
+    return 2
+
+  # calculate 
+  output = int(eating_cookies(n-1, memcache) + eating_cookies(n-2, memcache) + eating_cookies(n-3, memcache))
+  # set each value to memcache of n as it recurse
+  memcache[n] = output
+  print("memcache", memcache)
+  return output
+
+print(eating_cookies(10, memcache))
+print(memcache)
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
